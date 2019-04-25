dbefc6779426fafa8d63cd088acd7cd1af033625
Andreas Plesch <andreasplesch@users.noreply.github.com>
2019-04-25 15:04:58 -0400

    look for linux Unity

--- scripts/export-unity-package.sh
+++ scripts/export-unity-package.sh
@@ -1,13 +1,18 @@
-#! /bin/sh
+#!/bin/bash
+# invoke from repo root
 
 project_path=$(pwd)/UnityGLTF
-log_file=$(pwd)/build/unity-mac.log
+log_file=$(pwd)/build/unity-unix.log
 export_path=$(pwd)/current-package/UnityGLTF.unitypackage
+Unity=$(echo $HOME/Unity/Hub/Editor/*/Editor/Unity | gawk '{print $NF}')
+if [[ -a /Applications/Unity/Unity.app/Contents/MacOS/Unity ]]; then
+  Unity=/Applications/Unity/Unity.app/Contents/MacOS/Unity
+fi
 
 error_code=0
 
 echo "Creating package."
-/Applications/Unity/Unity.app/Contents/MacOS/Unity \
+$Unity \
   -batchmode \
   -nographics \
   -silent-crashes \
